758e5e5256c03e0bdeeb8fccd505d51f500364a9
SEDO11 <do11anm@naver.com>
2023-02-22 00:42:49 +0900

    20230222

--- movie/app/src/main/java/org/techtown/movie/ListViewItem.kt
+++ movie/app/src/main/java/org/techtown/movie/ListViewItem.kt
@@ -2,4 +2,4 @@ package org.techtown.movie
 
 import android.graphics.drawable.Drawable
 
-data class ListViewItem(val icon: Drawable, val title: String, val openingDate: String, val attendance: String)
+data class ListViewItem(val icon: Drawable?, val title: String?, val openingDate: String?, val attendance: String?)
--- movie/app/src/main/java/org/techtown/movie/SubActivity.kt
+++ movie/app/src/main/java/org/techtown/movie/SubActivity.kt
@@ -1,8 +1,11 @@
 package org.techtown.movie
 
 import android.annotation.SuppressLint
+import android.graphics.drawable.AdaptiveIconDrawable
+import android.media.Image
 import android.os.Build
 import android.os.Bundle
+import android.widget.ListView
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -13,21 +16,33 @@ import com.android.volley.toolbox.Volley
 
 class SubActivity : AppCompatActivity() {
 
-    private lateinit var tv : Any
+    private lateinit var listView2: ListView
+    private lateinit var adapter: ListViewAdapter
+    private lateinit var items: MutableList<ListViewItem>
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.subactivity)
 
+        // 레이아웃 파일에서 ListView를 찾아옵니다.
+        listView2 = findViewById(R.id.list_view)
+            // 아이템 리스트를 초기화합니다.
+        items = mutableListOf()
+
+        // 어댑터를 초기화합니다.
+        adapter = ListViewAdapter(items)
+
+        // ListView에 어댑터를 설정합니다.
+        listView2.adapter = adapter
+
         val movieCd = intent.getStringExtra("movieCode")
-        tv = findViewById<TextView>(R.id.tv1)
 
         if (movieCd != null) {
             jsonTask(movieCd)
         }
 
-        (tv as TextView?)!!.text = "영화 코드" + movieCd
+//        (tv as TextView?)!!.text = "영화 코드" + movieCd
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -38,20 +53,20 @@ class SubActivity : AppCompatActivity() {
         val key = BuildConfig.MOVIE_API_KEY
         val url = "https://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json?key=${key}&movieCd=${movieCd}"
 
+        var data = mutableListOf<String>()
+
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
                 // 전체 데이터 추출
                 val movieInfoResult = response.getJSONObject("movieInfoResult")
                 val movieInfo = movieInfoResult.getJSONObject("movieInfo")
-                val movieCd = movieInfo.getString("movieCd")
                 val movieNm = movieInfo.getString("movieNm")
                 val movieNmEn = movieInfo.getString("movieNmEn")
-                val movieNmOg = movieInfo.optString("movieNmOg")
                 val showTm = movieInfo.getString("showTm")
-                val prdtYear = movieInfo.optString("prdtYear")
                 val openDt = movieInfo.optString("openDt")
-                val prdtStatNm = movieInfo.optString("prdtStatNm")
-                val typeNm = movieInfo.optString("typeNm")
+
+                listItem(movieNm, movieNmEn, "러닝타임: " + showTm + "분  개봉일: " + openDt.slice(2..3) + "-" + openDt.slice(4..5)+ "-" + openDt.slice(6..7))
+
                 val nations = movieInfo.getJSONArray("nations")
                 val genres = movieInfo.getJSONArray("genres")
                 val directors = movieInfo.getJSONArray("directors")
@@ -65,6 +80,7 @@ class SubActivity : AppCompatActivity() {
                     val director = directors.getJSONObject(i)
                     val directorNm = director.getString("peopleNm")
                     directorNames.add(directorNm)
+                    listItem(directorNm, "감독", null)
                 }
 
                 // 출연 배우 정보 추출
@@ -73,6 +89,7 @@ class SubActivity : AppCompatActivity() {
                     val actor = actors.getJSONObject(i)
                     val actorNm = actor.getString("peopleNm")
                     actorNames.add(actorNm)
+                    listItem(actorNm, "배우", null)
                 }
 
                 // 제작사 정보 추출
@@ -81,24 +98,22 @@ class SubActivity : AppCompatActivity() {
                     val company = companys.getJSONObject(i)
                     val companyNm = company.getString("companyNm")
                     companyNames.add(companyNm)
+                    listItem(companyNm, "제작", null)
                 }
 
                 // 출력
                 println("영화 코드: $movieCd")
                 println("영화 제목: $movieNm")
                 println("영화 영문 제목: $movieNmEn")
-                println("영화 원제: $movieNmOg")
                 println("영화 재생 시간: $showTm 분")
-                println("제작 년도: $prdtYear")
                 println("개봉일: $openDt")
-                println("제작 상태: $prdtStatNm")
-                println("영화 유형: $typeNm")
                 println("제작 국가: $nations")
                 println("장르: $genres")
                 println("감독: $directorNames")
                 println("출연 배우: $actorNames")
                 println("등급: $audits")
                 println("제작사: $companyNames")
+
             },
             Response.ErrorListener { error ->
                 // 요청 실패 시 처리할 코드
@@ -107,4 +122,13 @@ class SubActivity : AppCompatActivity() {
 
         queue.add(jsonObjectRequest)
     }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun listItem(title:String?, subtitle1:String?, subtitle2:String?) {
+        val drawable = resources.getDrawable(R.drawable.ic_launcher_foreground, null)
+        val item = ListViewItem(drawable, title, subtitle1,  subtitle2)
+        items.add(item)
+        adapter.notifyDataSetChanged()
+    }
+
 }
